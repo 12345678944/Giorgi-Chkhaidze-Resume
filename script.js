5d4388dd824452628775547d69e379b0a9dbaf35
Giorgi Chkhaidze <giorgichkhaidze18@gmail.com>
2021-10-14 15:38:46 +0400

    auto desktop
    auto desktop

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-
-//if it's never been set or has been set to Mobile:
-if (Session["view"] == null || Session["view"].ToString() == "Mobile")
-{
-	viewporttag.Visible = false; //remove meta viewport tag
-	Session["View"] = "Desktop"; //we are now in desktop view
-	Desktop_button.Text = "Mobile View";
+var viewMode = getCookie("view-mode");
+if(viewMode == "desktop"){
+    viewport.setAttribute('content', 'width=1024');
+}else if (viewMode == "mobile"){
+    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no');
 }
-else
-{
-	viewporttag.Visible = true; //enable meta viewport tag for mobile views
-	Session["View"] = "Mobile";
-	Desktop_button.Text = "Desktop View";
-}
-     addClass(Desktop_button, "clicked");
